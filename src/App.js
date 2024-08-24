8ff791b6ee83662c8ea03142f72dcf96423f98b2
AnshulVermaQ <vermaanshul975@gmail.com>
2024-08-24 09:02:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-
+import Home from "./component/Home";
+import Cart from "./component/Cart";
+import Navbar from './component/Navbar';
+import {Provider} from "react-redux";
+import store from "./store/store";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Provider store = {store}>
+      <div>
+      <BrowserRouter>
+      <Navbar></Navbar>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
     </div>
+    </Provider>
   );
 }
 
---
+++ src/component/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Products from "./Products";
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to our Redux Store</h2>
+      <section>
+        <h1>Products</h1>
+        <Products></Products>
+      </section>
+    </div>
+  )
+}
+
+export default Home
